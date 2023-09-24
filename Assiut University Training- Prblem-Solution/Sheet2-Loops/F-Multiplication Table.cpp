c0d309f2b2a366467967b1d0c914a251f3d0db50
Tuhin Chakma <122518680+iamtuhinchakma@users.noreply.github.com>
2023-09-24 16:19:16 +0600

    Delete Assiut University Training- Prblem-Solution directory

--- Assiut University Training- Prblem-Solution/Sheet2-Loops/F-Multiplication Table.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-     int n;
-     cin >> n;
-     for( int i = 1; i <= 12; i++ ){
-        printf("%d * %d = %d\n", n, i, n * i);
-     }
-}
